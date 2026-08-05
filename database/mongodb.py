940ce348619184db5e3e234944279ec32f8747f2
abdulmalikansari180-ui <abdulmalikansari180@gmail.com>
2026-08-06 00:48:49 +0530

    Update mongodb.py

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,1 +1,12 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB_URI
 
+mongo_client = AsyncIOMotorClient(MONGO_DB_URI)
+
+db = mongo_client["IqraMusic"]
+
+users_db = db["users"]
+groups_db = db["groups"]
+playlist_db = db["playlists"]
+settings_db = db["settings"]
+stats_db = db["stats"]
