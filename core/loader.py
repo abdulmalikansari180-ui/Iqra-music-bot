4aa3862e4df69b475a598745a36d87b5a1e5514e
abdulmalikansari180-ui <abdulmalikansari180@gmail.com>
2026-08-06 00:50:14 +0530

    Update loader.py

--- core/loader.py
+++ core/loader.py
@@ -1,1 +1,18 @@
+import os
+import importlib
+import logging
 
+LOGGER = logging.getLogger("IqraMusic.Loader")
+
+
+def load_plugins():
+    plugins_path = "plugins"
+
+    for file in os.listdir(plugins_path):
+        if file.endswith(".py") and not file.startswith("__"):
+            module = f"plugins.{file[:-3]}"
+            try:
+                importlib.import_module(module)
+                LOGGER.info(f"✅ Loaded Plugin: {file}")
+            except Exception as e:
+                LOGGER.error(f"❌ Failed to load {file}: {e}")
