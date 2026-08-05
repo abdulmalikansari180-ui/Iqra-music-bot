5c8b5e27611489665b799f9aa64d6df5cf3d6475
abdulmalikansari180-ui <abdulmalikansari180@gmail.com>
2026-08-06 00:53:18 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,1 +1,9 @@
+from pyrogram import filters
+from pyrogram.types import Message
 
+from core.clients import app
+
+
+@app.on_message(filters.command("ping"))
+async def ping(_, message: Message):
+    await message.reply_text("🏓 Pong!\n\n✅ Iqra Music Bot is Working.")
