a4aa8d70d543c7836b1e707198b2a985a9432753
abdulmalikansari180-ui <abdulmalikansari180@gmail.com>
2026-08-06 00:35:57 +0530

    Update clients.py

--- core/clients.py
+++ core/clients.py
@@ -1,1 +1,12 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
+# Main Bot Client
+app = Client(
+    "IqraMusicBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=50,
+    sleep_threshold=30,
+)
