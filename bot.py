3bfe8f713659a54d75c03c214802ce7b413f808a
abdulmalikansari180-ui <abdulmalikansari180@gmail.com>
2026-08-05 21:35:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
+app = Client(
+    "IqraMusicBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@app.on_message()
+async def alive(_, message):
+    if message.text == "/alive":
+        await message.reply_text("✅ Iqra Music Bot is Online!")
+
+if __name__ == "__main__":
+    print("🚀 Starting Iqra Music Bot...")
+    app.run()
